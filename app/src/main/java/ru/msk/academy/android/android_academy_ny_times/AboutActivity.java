c85578d2d7398724ea8bb44d229500d2d93183ca
Maxim Shatalov <shatalov04@gmail.com>
2018-10-07 14:20:32 +0300

    initial commit

---
+++ app/src/main/java/ru/msk/academy/android/android_academy_ny_times/AboutActivity.java
@@ -0,0 +1,13 @@
+package ru.msk.academy.android.android_academy_ny_times;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
