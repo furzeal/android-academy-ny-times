059e1b9203565fa5a4029f02e1a7bc5ac69cc103
Maxim Shatalov <shatalov04@gmail.com>
2018-10-11 08:17:23 +0300

    add news classes

---
+++ app/src/main/java/ru/msk/academy/android/android_academy_ny_times/Category.java
@@ -0,0 +1,19 @@
+package ru.msk.academy.android.android_academy_ny_times;
+
+public class Category {
+    private final int id;
+    private final String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
